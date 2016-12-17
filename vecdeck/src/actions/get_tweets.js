cece89c7157791920e7dabb51c4821a43c15ed26
ericshape <wangjiprc@gmail.com>
2016-12-16 20:14:08 -0500

    Merge branch 'master' of https://github.com/ericshape/rnn_deck
    # Conflicts:
    #	.idea/jsLibraryMappings.xml
    #	vecdeck/src/actions/tweet.js

--- vecdeck/src/actions/get_tweets.js
+++ vecdeck/src/actions/get_tweets.js
@@ -55,3 +55,25 @@
 // }
 
 
+
+"use strict";
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+/**
+ * Schema
+ */
+var TweetSchema = new Schema({
+  claim_id: Number,
+  tweet_time: Date,
+  text: String,
+  html_text: String,
+  sent_vector: String,
+  opinion: String,
+  tweet_id: String
+}, {collection: 'tweet'});
+
+
+mongoose.model("Tweet", TweetSchema);
+
+
