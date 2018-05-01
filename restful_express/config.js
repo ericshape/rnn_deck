ea6abc03735990dda624f3ff57bee36056e9de1d
Ji Wang <wangjiprc@gmail.com>
2018-05-01 01:42:46 -0700

    Update config.js

--- restful_express/config.js
+++ restful_express/config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
 	twitter: {
-  	consumer_key: 'E27GYmNC16LLRsb7lMRlyusF1',
-		consumer_secret: 'FxiAgBdKZkO8iMwaf15CGnFAUvAfQZHQs0Sv11tsdTxAl6BpV6',
-		access_token: '71438797-w9X74pOqcGEE7kYHVMZ79OlttH1aKiZ7EMHJf4rZ6',
-		access_token_secret: 'eRAlFOwbjRj6meDxSXKL2Yh1OBejYXjAxv8Jz6F1zMD5k'
+  	consumer_key: 'XXX',
+		consumer_secret: 'XXX',
+		access_token: 'XXX',
+		access_token_secret: 'XXX'
 	}
 
 
